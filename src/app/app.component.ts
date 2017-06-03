dce4a60d0594a405775bf8adbd37297a89a37b67
Tibin Thomas <tibinthomas@outlook.com>
2017-06-03 09:43:50 +0530

    1) Understood important @Component attributes
    2) See comments in index.html and app.component.ts files for more details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  //selector: 'app-root',       //HTML tag selector
+  selector: '.app-root',        //By class
+  //selector: '[app-root]',     //HTML Tag atribute selector
+ //--------see the index.html file to see invocking------- of selectors
+
+  templateUrl: './app.component.html', 
+
+  template: `
+          <h1>This is App Component running here from the ts file</h1>
+  `,                                      //preference is given to this
+
+
+  styleUrls: ['./app.component.css'],     //Can add any no. of external css files 
+
+  styles: [`h1 {
+    color: dodgerblue;
+  }`]                                     //preference is given to this
 })
 export class AppComponent {
   title = 'app works!!';
